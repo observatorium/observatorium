14fc41114cd830e93472e2c1dd5a53dfdbe52614
Kemal Akkoyun <kakkoyun@gmail.com>
2020-01-30 18:12:50 +0100

    exit with error when first command fails

--- tests/test.sh
+++ tests/test.sh
@@ -4,12 +4,12 @@ set -e
 set -x
 set -o pipefail
 
-./kubectl wait --for=condition=available --timeout=10m -n minio deploy/minio || ./kubectl get pods --all-namespaces
-./kubectl wait --for=condition=available --timeout=10m -n observatorium deploy/thanos-querier || ./kubectl get pods --all-namespaces
+./kubectl wait --for=condition=available --timeout=10m -n minio deploy/minio || ./kubectl get pods --all-namespaces && exit 1
+./kubectl wait --for=condition=available --timeout=10m -n observatorium deploy/thanos-querier || ./kubectl get pods --all-namespaces && exit 1
 
 ./kubectl apply -f tests/manifests/observatorium-up.yaml
 
 sleep 5
 
 # This should wait for ~2min for the job to finish.
-./kubectl wait --for=condition=complete --timeout=5m -n default job/observatorium-up || ./kubectl get pods --all-namespaces
+./kubectl wait --for=condition=complete --timeout=5m -n default job/observatorium-up || ./kubectl get pods --all-namespaces && exit 1
