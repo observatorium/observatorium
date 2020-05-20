c1ddfc239384c9150cc5439649aa1ae681a7931c
Fred Rolland <frolland@redhat.com>
2020-05-20 12:09:25 +0300

    Remove unused files (#278)

--- fmt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# This script uses jsonnetfmt to format jsonnet files.
-
-find . -type f -not -path './*vendor/*' \( -name '*.libsonnet' -o -name '*.jsonnet' \) | xargs -L1 jsonnetfmt -n 2 --max-blank-lines 2 --string-style s --comment-style s -i
-
