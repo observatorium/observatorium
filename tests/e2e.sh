3ef8c277206bb0f28a6a7a1d23db2bf980662977
Matthias Loibl <mail@matthiasloibl.com>
2020-09-17 16:14:13 +0200

    Delete leftovers from the Operator (#344)
    * Delete leftovers from the Operator
    
    * Remove make example/manifests target
    
    * Commit jsonnetfile.lock.json going forward
    
    We need to pin the dependencies explicitly again, otherwise the
    dependencies might change without an explicit update and CI will fail.

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -33,30 +33,6 @@ deploy() {
     $KUBECTL apply -f environments/dev/manifests/
 }
 
-wait_for_cr() {
-    observatorium_cr_status=""
-    target_status="Finished"
-    timeout=$true
-    interval=0
-    intervals=600
-    while [ $interval -ne $intervals ]; do
-      echo "Waiting for" $1 "currentStatus="$observatorium_cr_status
-      observatorium_cr_status=$($KUBECTL -n observatorium get observatoria.core.observatorium.io $1 -o=jsonpath='{.status.conditions[*].currentStatus}')
-      if [ "$observatorium_cr_status" = "$target_status" ]; then
-        echo $1 CR status is now: $observatorium_cr_status
-	    timeout=$false
-	    break
-	  fi
-	  sleep 5
-	  interval=$((interval+5))
-    done
-
-    if [ $timeout ]; then
-      echo "Timeout waiting for" $1 "CR status to be " $target_status
-      exit 1
-    fi
-}
-
 run_test() {
     local suffix
     while [ $# -gt 0 ]; do
