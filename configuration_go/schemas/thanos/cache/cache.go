86e0745724f340bb4af014a2a8110efbd6518799
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2023-09-21 21:25:01 +0200

    fix cache generics
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- configuration_go/schemas/thanos/cache/cache.go
+++ configuration_go/schemas/thanos/cache/cache.go
@@ -1,24 +1,34 @@
 package cache
 
 import (
+	"github.com/bwplotka/mimic"
 	"github.com/observatorium/observatorium/configuration_go/schemas/thanos/cache/memcached"
 	"github.com/observatorium/observatorium/configuration_go/schemas/thanos/cache/memory"
 	"github.com/observatorium/observatorium/configuration_go/schemas/thanos/cache/redis"
+	"gopkg.in/yaml.v2"
 )
 
-type cacheConfig interface {
+// IndexCacheConfig specifies the index cache config.
+type IndexCacheConfig struct {
+	ConfigType string      `yaml:"type"`
+	Config     interface{} `yaml:"config"`
+}
+
+func (c IndexCacheConfig) String() string {
+	ret, err := yaml.Marshal(c)
+	if err != nil {
+		mimic.Panicf("error mashalling IndexCacheConfig to yaml: %v", err)
+	}
+	return string(ret)
+}
+
+type CacheConfig interface {
 	redis.RedisClientConfig | memcached.MemcachedClientConfig | memory.MemoryCacheConfig
 	Type() string
 }
 
-// IndexCacheConfig specifies the index cache config.
-type IndexCacheConfig[T cacheConfig] struct {
-	ConfigType string `yaml:"type"`
-	Config     T      `yaml:"config"`
-}
-
-func NewConfig[T cacheConfig](c T) *IndexCacheConfig[T] {
-	return &IndexCacheConfig[T]{
+func NewConfig[T CacheConfig](c T) *IndexCacheConfig {
+	return &IndexCacheConfig{
 		ConfigType: c.Type(),
 		Config:     c,
 	}
